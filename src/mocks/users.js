51410e34376d00e4aaea9327a9064e7bbc961aa3
luneciux <jejosu346@gmail.com>
2024-04-21 14:12:53 -0300

    criando estrutura basica da API e resolvendo as pendencias de depreciacao do curso

---
+++ src/mocks/users.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Josue1'
+    },
+
+    {
+        id: 2,
+        name: 'Josue2'
+    },
+
+    {
+        id: 3,
+        name: 'Josue3'
+    },
+]
